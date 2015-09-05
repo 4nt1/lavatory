0f44aaa47fee9ad0b9e2f76ed4663a4063113e80
4nt1 <antoinemary@hotmail.fr>
2015-09-05 23:12:13 +0200

    meh

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :stage, :production
-server 'lavatory.antoinemary.me', user: 'deploy', roles: %w{app web}
+server 'varanasi.antoinemary.me', user: 'deploy', roles: %w{app web}
 set :branch, "master"
