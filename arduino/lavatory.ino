3780bae02564d8d5a2b82008fb291caf56bbdec9
4nt1 <antoinemary@hotmail.fr>
2015-09-06 12:15:50 +0200

    meh

--- arduino/lavatory.ino
+++ arduino/lavatory.ino
@@ -1,12 +1,59 @@
-char serverUrl[] = "http://antoinemary.me";
-const int sensorPin  = A0;
+#include <SPI.h>
+#include <Ethernet.h>
 int sensorValue;
-int sensorHigh       = 0;
-int sensorLow        = 1023;
+const int sensorPin   = A0;
+int sensorHigh        = 0;
+int sensorLow         = 1023;
+byte mac[]            = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
+IPAddress ip(192,168,1,6);
+EthernetClient client;
+
+void sendRequest(EthernetClient client, char url[]) {
+  char error;
+  Serial.println(client);
+  Serial.println(url);
+  error = client.connect("lavatory.antoinemary.me", 80);
+  if(error == 1) {
+      Serial.println("Connexion OK, envoi en cours...");
+      client.println(url);
+      client.println("Host: lavatory.antoinemary.me");
+      client.println("Connection: close");
+      client.println();
+  } else {
+    Serial.println("Echec de la connexion");
+    Serial.println("Code erreur");
+    Serial.println(error);
+    switch(error) {
+      case(-1):
+        Serial.println("Time out");
+        break;
+      case(-2):
+        Serial.println("Serveur invalide");
+        break;
+      case(-3):
+        Serial.println("Tronque");
+        break;
+      case(-4):
+        Serial.println("Reponse invalide");
+        break;
+    }
+    while(1);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
+  char error = 0;
+  error = Ethernet.begin(mac);
+
+  if (error == 0) {
+    Serial.println("DHCP failed.");
+    Ethernet.begin(mac, ip);
+  }
+
+  delay(1000);
+  Serial.println("Ethernet ready.");
 
   while(millis() < 10000) {
     sensorValue = analogRead(sensorPin);
@@ -17,12 +64,20 @@ void setup() {
       sensorLow = sensorValue;
     }
   }
+
+  Serial.println("Sensor calibrated.");
 }
 
 void loop() {
   sensorValue = analogRead(sensorPin);
   delay(2000);
-  Serial.print("Valeur du capteur :\n");
-  Serial.print(sensorValue);
-  Serial.print("\n");
+  Serial.println("Valeur du capteur :");
+  Serial.println(sensorValue);
+  if (sensorValue > sensorHigh * 0.9) {
+    Serial.println("on");
+    sendRequest(client, "GET /on HTTP/1.1");
+  } else {
+    Serial.println("off");
+    sendRequest(client, "GET /off HTTP/1.1");
+  }
 }
